ceb1f650b0e65f0d8313f3ab044351975f65815b
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2025-06-09 13:02:23 -0400

    initial settings for projects

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+export function setupServer() {
+  const app = express();
+
+  const PORT = 3000;
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is runing on port ${PORT}`);
+  });
+}
