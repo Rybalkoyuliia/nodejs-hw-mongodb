7e9eb05a7d59348395787e838ff12bb851ee4bd5
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2025-06-09 16:33:28 -0400

    fixed envKeys

--- src/constants/envkeys.js
+++ src/constants/envkeys.js
@@ -4,3 +4,7 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 dotenv.config();
 
 export const PORT = Number(getEnvVar('PORT', '3000'));
+export const user = getEnvVar('MONGODB_USER');
+export const password = getEnvVar('MONGODB_PASSWORD');
+export const url = getEnvVar('MONGODB_URL');
+export const db = getEnvVar('MONGODB_DB');
